f875189a9f712892b376356a6d6b37c07df1446b
zainahda <zainahda@gmail.com>
2022-09-18 19:35:16 +0700

    Tugas promise DTS Kominfo

--- promise.js
+++ promise.js
@@ -1,7 +1,18 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+
+  try {
+    const theaterOne = await promiseTheaterIXX();
+    const theaterTwo = await promiseTheaterVGC();
+
+    const output = [...theaterOne, ...theaterTwo].filter((value) => value.hasil === emosi).length;
+    return output;
+  } catch(error) {
+    console.log(error)
+  }
+}
 
 module.exports = {
   promiseOutput,
